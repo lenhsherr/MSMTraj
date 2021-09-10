bc3cf5721b0c70b323ffbae2c71812f8b19cbd98
rob arbon <robert.arbon@bristol.ac.uk>
2021-09-10 17:49:45 +0100

    trajectory class working for reading (not parquet though)

--- FOAM/tests/test_trajectory.py
+++ FOAM/tests/test_trajectory.py
@@ -6,12 +6,15 @@ import pytest
 
 from  FOAM.trajectory import Trajectory
 
-DATA_DIR = 'FOAM/data'
+DATA_DIR = 'FOAM/data/'
 
 def test_load_existing_h5():
     config = {'coordinates_path': DATA_DIR + 'alanine-dipeptide-0-250ns-nowater.h5'}
     traj = Trajectory(config)
-    assert traj.coordinates_path == config['coordinates_path']
+    assert traj.coordinates_path is not None
 
-
-def 
+def test_read_existing_h5():
+    config = {'coordinates_path': DATA_DIR + 'alanine-dipeptide-0-250ns-nowater.h5'}
+    traj = Trajectory(config)
+    traj.read()
+    assert traj.coordinates is not None
--- FOAM/trajectory.py
+++ FOAM/trajectory.py
@@ -2,8 +2,10 @@ import mdtraj as md
 import numpy as np
 from pathlib import Path
 from typing import Union, Dict
+import pandas as pd
 
-SELF_CONTAINED_FMTS = ['h5', 'hdf5', 'npy', 'npz']
+
+SELF_CONTAINED_FMTS = ['h5', 'hdf5', 'npy', 'npz', 'parquet']
 
 
 class Trajectory(): 
@@ -12,15 +14,30 @@ class Trajectory():
         self._topology_path: Path = None
         self._coordinates: np.ndarray = None
         self._timestep: float = None
-
+        self._file_type: str = None
         self._parse_configuration(configuration)
 
+    @property
+    def file_type(self) -> str:
+        return self._file_type
+
+    @file_type.setter
+    def file_type(self, value) -> None:
+        self._file_type = value
+
+    @property
+    def coordinates(self) -> np.ndarray:
+        return self._coordinates
+
+    @coordinates.setter
+    def coordinates(self, value) -> None: 
+        self._coordinates = value
 
     @property
     def coordinates_path(self) -> Path:
         return self._coordinates_path
 
-    @property.setter
+    @coordinates_path.setter
     def coordinates_path(self, value) -> None:
         try: 
             value = Path(value)
@@ -32,7 +49,7 @@ class Trajectory():
     def topology_path(self) -> Path:
         return self._topology_path
     
-    @property.setter
+    @topology_path.setter
     def topology_path(self, value) -> None: 
         try: 
             value = Path(value)
@@ -40,28 +57,29 @@ class Trajectory():
             raise TypeError('Topology path must be str or Path type')
         self._topology_path = value
         
-    def _parse_configuation(self, configuration: Dict[str, Union[str, Path, None]]) -> None: 
-        self.coordinates_path = configuration['configuation_path']
-        file_type = self.coordinates_path.suffix
-        if not file_type in SELF_CONTAINED_FMTS: 
+    def _parse_configuration(self, configuration: Dict[str, Union[str, Path, None]]) -> None: 
+        self.coordinates_path = configuration['coordinates_path']
+        self.file_type = self.coordinates_path.suffix[1:]
+        
+        if not (self.file_type in SELF_CONTAINED_FMTS): 
             try: 
                 self.topology_path = configuration['topology_path']
             except KeyError:
-                raise KeyError(f'topology_path needed for coordinate files of type {file_type}')
-        
+                raise KeyError(f'topology_path needed for coordinate files of type {self.file_type}')
 
+    def read(self) -> None:
+        if self.file_type in ['h5', 'hdf5', 'xtc', 'pdb']:
+            kwargs = {}
+            if not (self.file_type in SELF_CONTAINED_FMTS):
+                kwargs['top'] = str(self.topology_path)
 
-    # def _parse_configuration(self, config: Mapping) -> None: 
-    #     self.coordinates_path = Path(config['coordinates_path'])
-    #     if 'topology_path' in config.keys(): 
-    #         if config['topology_path'] is not None: 
-    #             self.topology_path = Path(config['topology_path'])
-    #         else: 
-    #             raise ValueError('topology_path can not be None')
+            traj = md.load(str(self.coordinates_path), **kwargs)
+            n_frames = traj.n_frames
+            self.coordinates = traj.xyz.reshape(n_frames, -1)
 
-    # def _requires_topology(self):
-    #     return not self.coordinates_path.suffix in SELF_CONTAINED_FMTS
-    
+        elif self.file_type in ['npy']: 
+            traj = np.load(self.coordinates_path)
+            self.coordinates = traj
 
-    # def read(self) -> None:
-
+        else: 
+            raise NotImplementedError(f"file type {self.file_type} is not supported yet.")
